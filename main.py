7e79db735c3c85880ca7116ab7c996b59ef8534b
Serdan1 <danielserranoreal@gmail.com>
2025-10-04 11:38:03 +0200

    ejecutar desde main

--- main.py
+++ main.py
@@ -2,9 +2,5 @@
 from src.gui import DigitRecognizerGUI
 
 if __name__ == "__main__":
-    app = DigitRecognizerGUI(
-        model_path="model.h5",
-        firebase_cred="proyecto-mnist-dashboard-firebase-adminsdk-fbsvc-57d1f6e9be.json",
-        firebase_bucket="proyecto-mnist-dashboard.firebasestorage.app"
-    )
+    app = DigitRecognizerGUI(model_path="model.h5")
     app.run()
